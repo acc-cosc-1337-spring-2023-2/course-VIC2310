d4783827b775da0e2f983bc88dec27331b51eb40
VIC2310 <123132006+VIC2310@users.noreply.github.com>
2023-02-11 23:43:57 +0000

    VIC2310

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,13 +1,21 @@
-#include "expressions.h"
+#include "hwexpressions.h"
 
 //example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
+//int add_numbers(int num1, int num2)
+//{
+//	return num1 + num2;
+//}
 
 //write function code here
 
+double get_sales_tax_amount(double meal_amount)
+{
+	return tax_rate * meal_amount;
+}
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
 
 
